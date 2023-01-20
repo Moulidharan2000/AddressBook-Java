254cb19b980e2b366f2fe2ee774dc1aab2baba56
Moulidharan2000 <smoulidharanmech@gmail.com>
2023-01-20 14:27:42 +0530

    Added : Create a Contacts in AddressBook System

---
+++ src/com/bridgelabz/addressbook/Contacts.java
@@ -0,0 +1,24 @@
+package com.bridgelabz.addressbook;
+
+public class Contacts {
+	
+	static void Contact() {
+	
+		String firstName = "Raj";
+		String lastName = "Kumar";
+		String address = "Anna Nagar";
+		String city = "Chennai";
+		String state = "TamilNadu";
+		int zipCode = 600028;
+		long phoneNumber = 635423410;
+		String email = "rajkumar@gmail.com";
+		
+		System.out.println("Name :"+firstName+" "+lastName);
+		System.out.println("Address : "+address);
+		System.out.println("City : "+city);
+		System.out.println("State : "+state);
+		System.out.println("ZipCode : "+zipCode);
+		System.out.println("Phone Number : "+phoneNumber);
+		System.out.println("Email ID : "+email);
+	}
+}
